3ed57562018ded948b9f851265ad895ca6d6dc36
Fergasi <fergusmagor@gmail.com>
2022-02-13 23:11:22 -0500

    v2

--- getLastItemFrom.js
+++ getLastItemFrom.js
@@ -1,8 +1,9 @@
 const prompt = require('prompt-sync')();
 
 var n = new Array();
-let last = n[n.length-1];
 
 n = (prompt("Enter a couple of numbers seperated by commas and hit enter: ").split(","));
+
+let last = n[n.length-1];
 
 console.log(last);
